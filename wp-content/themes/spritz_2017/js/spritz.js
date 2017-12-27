35276e233c89bb0b4ece6f530ab2ee78471e1dd5
Sébastien PAQUET <cavallinox@MacBook-Pro-de-Sebastien-2.local>
2017-12-27 19:46:36 +0100

    +1

--- wp-content/themes/spritz_2017/js/spritz.js
+++ wp-content/themes/spritz_2017/js/spritz.js
@@ -5,10 +5,10 @@ jQuery( "#boutton" ).click(
 	}
 );
 
+// gestion de l'apparition du champs recherche
 jQuery(document).ready(function() {
 	jQuery('#recherche').hide();
 })
-
 
 function zone_search() {
     var x = document.getElementById("recherche");
@@ -19,3 +19,90 @@ function zone_search() {
         x.style.display = "none";
     }
 }
+
+// Fonction Scroll to top
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+});
+
+
+// Fonction Stellar : Parallax
+	var parallax = function() {
+		jQuery(window).stellar();
+	};
+	
+	
+// Lancement des fonctions.
+	jQuery(function(){
+
+		parallax();
+
+	});
+	
+//Slider Bootstrap
+(function( $ ) {
+
+    //Function to animate slider captions 
+	function doAnimations( elems ) {
+		//Cache the animationend event in a variable
+		var animEndEv = 'webkitAnimationEnd animationend';
+		
+		elems.each(function () {
+			var $this = $(this),
+				$animationType = $this.data('animation');
+			$this.addClass($animationType).one(animEndEv, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+	
+	//Variables on page load 
+	var $myCarousel = $('#carousel-example-generic'),
+		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+		
+	//Initialize carousel 
+	$myCarousel.carousel();
+	
+	//Animate captions in first slide on page load 
+	doAnimations($firstAnimatingElems);
+	
+	//Pause carousel  
+	$myCarousel.carousel('pause');
+	
+	
+	//Other slides to be animated on carousel slide event 
+	$myCarousel.on('slide.bs.carousel', function (e) {
+		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+		doAnimations($animatingElems);
+	});  
+    $('#carousel-example-generic').carousel({
+        interval:3000,
+        pause: "false"
+    });
+	
+})(jQuery);	
---
+++ wp-content/themes/spritz_2017/search.php
@@ -0,0 +1,75 @@
+<?php 
+/**
+ * The template for displaying search results pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Spritz 2017
+ *
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+ get_header(); 
+ 
+ ?>
+ 
+ 	<!-- heading de la page, fond parallax -->
+ 	<div class="container-fluid">
+		<div class="row">
+			<div class="page-heading" data-section="home" style="background-image: url(<?php echo $GLOBALS['image_defaut']; ?>);" data-stellar-background-ratio="0.5">
+			
+					<article id="post-<?php the_ID(); ?> <?php post_class(); ?>">
+							<div class="spritz-title-holder">
+								<div class="spritz-container clearfix">
+									<div class="spritz-container-inner">
+										<div class="spritz-title-subtitle-holder">
+											<h1><?php pll_e("Recherche"); ?></h1>
+										</div>
+							  		</div>
+								</div>
+							</div>
+					</article>
+					
+			</div>
+			
+		</div>
+	</div>
+	
+	<!-- Corps de la page -->
+	<div class="container">
+		<div class="row">
+			<main class="content-area col-md-12">
+				
+				<div class="heading-title left infos-film">
+					<h3><?php pll_e("Résultats de votre recherche"); ?></h3>
+					
+					<div class="separateur"></div>
+					
+				</div>
+
+				<div class="col-md-12">
+					
+					<ol>
+				
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					
+						<li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+						
+					<?php endwhile; else: ?>
+						    <div class="alert alert-warning" role="alert">
+							  <?php echo $GLOBALS['message_alerte']; ?>
+							</div>
+					<?php endif; ?>
+					
+					</ol>
+				</div>
+						
+			</main>
+			
+		</div>
+	</div>
+	
+<?php get_footer(); ?>
